f6d879118a9e3377bf5f42d5561d4640d0be1a8f
Mattia V. <56953148+mvailati84@users.noreply.github.com>
2024-09-29 20:29:12 +0200

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -97,7 +97,7 @@ function showNextDay() {
 function getWeekNumberWithReference(date) {
   const millisecondsInDay = 24 * 60 * 60 * 1000;
   // This reference date represents the week 4
-  const referenceDate = new Date("2024-03-25T00:00:00");
+  const referenceDate = new Date("2024-09-30T00:00:00");
 
   date = shiftToMonday(date);
 
